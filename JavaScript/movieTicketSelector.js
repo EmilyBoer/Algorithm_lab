7794fc4a8a8908056c66a7e1bf224c03d0beb69f
EmilyBoer <Emily.boerner@gmail.com>
2015-07-13 12:05:14 -0700

    Add marilyn movie selector

--- JavaScript/movieTicketSelector.js
+++ JavaScript/movieTicketSelector.js
@@ -1,17 +1,2 @@
-var showingTime = [["Die hard", 445] ["Castaway", 700] ["Minions", 1200]]
+var newReleases = [["Amy", 445] ["End of the Tour", 700] ["Tanjarine", 1200]]
 
-var notShowing = ["American Hustle", "Speed", "Amy", "End of the Tour"]
-
-movieChoice = "Speed"
-
-var ticket = buildTicket(showingTime, notShowing, movieChoice )
-
-playTime.map(function(movie) {
-  if (movie == playTime][0]); {
-return ("your movie is playing at" + playtime[1])
-  }
-  else if  (movie == notShowing[0]); {
-return ("your movie is not playing tonight")
-  }
-
-};
