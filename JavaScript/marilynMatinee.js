02dc96ef8e0d77c3d99098bc449815e6f09eb163
EmilyBoer <Emily.boerner@gmail.com>
2015-07-13 12:46:55 -0700

    All instance to marilynMatinee

--- JavaScript/marilynMatinee.js
+++ JavaScript/marilynMatinee.js
@@ -1,16 +1,24 @@
 // A different Marilyn Monroe movie plays at noon every day. Plug in your Marilyn Monroe movie of choice to see if it's playing today.
 
+function movie (allMovies, yourPick) {
+if (allMovies[0] == yourPick){
+  var todaysMarilyn = allMovies.shift();
+  return function(){ alert(todaysMarilyn + "is today's matinee!");
+};
+} else {
+return function() { alert("Sorry, "+ allMovies[0] + "is playing today. Try again tomorrow." );
+}
+}
+}
+
 
 var marilynMovies = ["Some Like it Hot", "The Seven Year Itch", "Gentlemen Prefer Blondes", "The Misfits", "Bus Stop", "Niagra", "All About Eve" ]
 
 var yourChoice = "Some Like it Hot";
 
-function movie (dailyMarilyn, yourChoice) {
-if (dailyMarilyn[0] == yourChoice){
-  var todaysMarilyn = marilynMovies.shift();
-  return function ( ) { alert(yourChoice + "is today's matinee!");
-};
-} else {
-return function() { alert("Sorry, "+ dailyMarilyn[0] + "is playing today. Try again tomorrow." );
-}
-}
+
+var ticket = movie(marilynMovies, yourChoice);
+ticket();
+
+
+
--- JavaScript/movieTicketSelector.js
+++ JavaScript/movieTicketSelector.js
@@ -1,2 +1,10 @@
-var newReleases = [["Amy", 445] ["End of the Tour", 700] ["Tanjarine", 1200]]
+// You want to go see a movie. To check to showtimes, you to iterate through the 2nd elements of the sub-arrays and make sure the movie time is at least 20 minutes later than the current hour (it's a 20 minute drive to the theater).
+
+// pseudo code
+-create an array of shows and movie times
+-define a variable for the current time
+
+
+var movieTimes = [["Amy", 445], ["End of the Tour", 700], ["Magic Mike XXL"] ["Tanjarine", 1200]]
+
 
