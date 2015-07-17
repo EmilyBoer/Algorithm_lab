781e2fa02ed7f67c9926c4e509d05c87e0a96c17
EmilyBoer <Emily.boerner@gmail.com>
2015-07-16 23:13:54 -0700

    Organize folder

--- JavaScript/marilynMatinee.js
+++ JavaScript/marilynMatinee.js
@@ -1,15 +1,17 @@
 // A different Marilyn Monroe movie plays at noon every day. Plug in your Marilyn Monroe movie of choice to see if it's playing today.
 
-function movie (allMovies, yourPick) {
+function movie (allMovies, yourPick){
 if (allMovies[0] == yourPick){
   var todaysMarilyn = allMovies.shift();
-  return function(){ alert(todaysMarilyn + "is today's matinee!");
-};
+  return function(){
+    alert("todaysMarilyn is today's matinee!");
+  };
 } else {
-return function() { alert("Sorry, "+ allMovies[0] + "is playing today. Try again tomorrow." );
+return function(){
+  alert("Sorry, "+ allMovies[0] + "is playing today. Try again tomorrow." );
 }
 }
-}
+
 
 
 var marilynMovies = ["Some Like it Hot", "The Seven Year Itch", "Gentlemen Prefer Blondes", "The Misfits", "Bus Stop", "Niagra", "All About Eve" ]
