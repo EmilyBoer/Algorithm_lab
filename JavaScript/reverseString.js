d2022b2f1c0e48dad559ff63041b1e54e87e0a25
EmilyBoer <Emily.boerner@gmail.com>
2015-07-29 19:03:05 -0700

    add reverseString

---
+++ JavaScript/reverseString.js
@@ -0,0 +1,23 @@
+// Take an input of a string and return the same string reversed
+
+
+// learning compentencies
+//   working through a javascript problem
+//   for loop
+//   conditional statements
+//   typeof method
+
+
+
+var reverse = function(str){
+if typeof str === “string” {
+var results = ‘ ‘;
+var letters = str.split(” ”);
+for (var i = letters.length- 1; i. > 0; I - -){
+result = letters[i];
+};
+result result;
+}else {
+console.log(“error”)
+};
+};
