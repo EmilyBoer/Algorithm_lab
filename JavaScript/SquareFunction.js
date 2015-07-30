aba760ba4c210fa9ef3c4ab90444d1c4a3efd7dc
EmilyBoer <Emily.boerner@gmail.com>
2015-07-29 18:16:30 -0700

    add feature

--- JavaScript/SquareFunction.js
+++ JavaScript/SquareFunction.js
@@ -1,7 +1,7 @@
 // A function that uses the map method to square each number of an array.
 
 
-var numbers = [1,2,3,4,5];
+var numbers = [1,2,3,4,5,6];
 
 var results = numbers.map(function(eachNumber){
   return eachNumber * eachNumber;
