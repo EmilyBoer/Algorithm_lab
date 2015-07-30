cb58009238722f7fdb3f46ef68c1fac2a21f6fac
EmilyBoer <Emily.boerner@gmail.com>
2015-07-29 19:35:44 -0700

    add characterFrequency

---
+++ JavaScript/characterFrequency.js
@@ -0,0 +1,25 @@
+// Take a string of characters and determine how often each letter appears
+
+// learning competencies
+
+// pseudo code
+// input = String
+// output = string and number pairs
+
+// make sure you're taking a string
+
+var characterFrequency = function(str){
+  var letters = {}
+  if (typeof str === "str"){
+    for (var i= 0; str.length > i; i++ ){
+      if (letters [str[i]]){
+        letters [str[i]] +=1;
+      } else {
+        letters[str[i]] = 1;
+      }
+    }
+  }
+};
+
+
+console.log(characterFrequency("hello"));
